732dc88eeab8bb621bd6ffc94b424bc052c8e85d
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2025-01-13 13:21:04 +0100

    fix - userModel

--- resources/js/models/UserModel.ts
+++ resources/js/models/UserModel.ts
@@ -4,6 +4,7 @@ import { IdAndTimestamp } from "./IdAndTimestamp";
 export interface UserModel extends IdAndTimestamp {
   name: string;
   email: string;
+  email_verified_at: string;
   active_client_sessions: ClientModel[];
   clients: ClientModel[];
   role: string;
